7a9cfcbcd110662c70f0344f88b1417cbabe2d8d
marclevetin <marc@derbyhat.org>
2017-07-22 13:54:38 -0400

    save point after all methods are done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -27,32 +27,66 @@ class StaticPagesController < ApplicationController
 
   def battle(battle_data)
     @battle_data = battle_data
+    round = 0
     while @battle_data.defend_models > 0 do
       to_hit
       to_wound
       saving_throw
+      round += 1
+      if round > 20
+        break
+      end
     end
-  #   X units fire Y shots a piece
-  #   Ballistic strength = for every roll higher or equal than this number, it's a hit
-  #
-  #   Wounding....
-  #   if a hit:
-  #     if weapon strgnth's equal to defending toughness, 4 to wound.
-  #     elsif greater than, but not double = 3 to wound
-  #     elsif double or higher = 2 to wound
-  #       if you're 1 below toughness, 5 to wound
-  #         if you're double below toughness, 6 to wound
-  #
-  #
-  # To see if wounds stick, check armour value
-  #   if armour is X, you need a X or higher to save.
+    return round
   end
+
   def to_hit
     @shots = @battle_data.attack_models * @battle_data.shots
     @rolled_shots = roll(@shots)
     @rolled_shots.delete_if { |hit| hit < @battle_data.ballistic_strength }
     @hits = @rolled_shots.length
     end
+  end
+
+  def to_wound
+    if @hits > 0
+      @wounds = roll(@hits)
+      if @battle_data.strength >= @battle_data.toughness * 2
+        @wounds.delete_if { |wound| wound < 2 }
+      elsif @battle_data.strength > @battle_data.toughness
+        @wounds.delete_if { |wound| wound < 3 }
+      elsif @battle_data.strength == battle_data.toughness
+        @wounds.delete_if { |wound| wound < 4 }
+      elsif @battle_data.strength / 2 <= battle_data.toughness
+        @wounds.delete_if { |wound| wound < 6 }
+      elsif @battle_data.strength < battle_data.toughness
+        @wounds.delete_if { |wound| wound < 5 }
+      else
+        "error"
+      end
+    end
+    return @wounds.length
+  end
+
+  def saving_throw
+    if @wounds > 0
+      @saved = roll(@wounds)
+      @original = @saved.length
+      @saved.delete_if { |save| save < @battle_data.armour }
+      if @battle_data.ap_value > 0
+        @saved.delete_if { |save| save - @battle_data.ap_value < @battle_data.armour  }
+      end
+    end
+    @damaged = @original - @saved.length
+    while @damage > 0
+      current_wounds = 0
+      current_wounds += @battle_data.damage
+        if current_wounds >= @battle_data.wounds
+          @battle_data.defend_models -=1
+          current_wounds = 0
+        end
+    end
+    return @battle_data.defend_models
   end
 
   def roll(num)
