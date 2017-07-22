ebcfcd90e9600b3e6cd508727643f0e7c2ecc6ad
marclevetin <marc@derbyhat.org>
2017-07-22 15:18:27 -0400

    backend is working.  Ready to start with charts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,14 @@ class StaticPagesController < ApplicationController
   end
 
   def create
-    battle(battle_params)
+    @total_rounds = []
+    @times_run = params["times_run"].to_i
+    while @times_run > 0
+      battle(battle_params)
+      @times_run -=1
+      @total_rounds << @round
+    end
+    render "index"
   end
 
   private
@@ -21,45 +28,42 @@ class StaticPagesController < ApplicationController
       :defend_models,
       :toughness,
       :wounds,
-      :armour
+      :armour,
+      :times_run
     )
   end
 
   def battle(battle_data)
     @battle_data = battle_data
-    round = 0
-    while @battle_data.defend_models > 0 do
+    @round = 0
+    while @battle_data["defend_models"].to_i > 0 do
       to_hit
       to_wound
       saving_throw
-      round += 1
-      if round > 20
-        break
-      end
+      @round += 1
     end
-    return round
+    return @round
   end
 
   def to_hit
-    @shots = @battle_data.attack_models * @battle_data.shots
+    @shots = @battle_data["attack_models"].to_i * @battle_data["shots"].to_i
     @rolled_shots = roll(@shots)
-    @rolled_shots.delete_if { |hit| hit < @battle_data.ballistic_strength }
+    @rolled_shots.delete_if { |hit| hit < @battle_data["ballistic_strength"].to_i }
     @hits = @rolled_shots.length
-    end
   end
 
   def to_wound
     if @hits > 0
       @wounds = roll(@hits)
-      if @battle_data.strength >= @battle_data.toughness * 2
+      if @battle_data["strength"].to_i >= @battle_data["toughness"].to_i * 2
         @wounds.delete_if { |wound| wound < 2 }
-      elsif @battle_data.strength > @battle_data.toughness
+      elsif @battle_data["strength"].to_i > @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 3 }
-      elsif @battle_data.strength == battle_data.toughness
+      elsif @battle_data["strength"].to_i == @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 4 }
-      elsif @battle_data.strength / 2 <= battle_data.toughness
+      elsif @battle_data["strength"].to_i / 2 <= @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 6 }
-      elsif @battle_data.strength < battle_data.toughness
+      elsif @battle_data["strength"].to_i < @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 5 }
       else
         "error"
@@ -69,24 +73,29 @@ class StaticPagesController < ApplicationController
   end
 
   def saving_throw
-    if @wounds > 0
-      @saved = roll(@wounds)
+    if @wounds.length > 0
+      @saved = roll(@wounds.length)
       @original = @saved.length
-      @saved.delete_if { |save| save < @battle_data.armour }
-      if @battle_data.ap_value > 0
-        @saved.delete_if { |save| save - @battle_data.ap_value < @battle_data.armour  }
+      @saved.delete_if { |save| save < @battle_data["armour"].to_i }
+      if @battle_data["ap_value"].to_i > 0
+        @saved.delete_if { |save| save - @battle_data["ap_value"].to_i < @battle_data["armour"].to_i  }
+      end
+      if @saved.nil?
+        @damaged = @original
+      else
+        @damaged = @original - @saved.length
+      end
+      while @damaged > 0
+        current_wounds = 0
+        current_wounds += @battle_data["damage"].to_i
+          if current_wounds >= @battle_data["wounds"].to_i
+            @battle_data["defend_models"] = @battle_data["defend_models"].to_i - 1
+            current_wounds = 0
+          end
+          @damaged -= 1
       end
     end
-    @damaged = @original - @saved.length
-    while @damage > 0
-      current_wounds = 0
-      current_wounds += @battle_data.damage
-        if current_wounds >= @battle_data.wounds
-          @battle_data.defend_models -=1
-          current_wounds = 0
-        end
-    end
-    return @battle_data.defend_models
+    return @battle_data["defend_models"].to_i
   end
 
   def roll(num)
