428763a8366a7b48c3f153ec90c150e99a3b997f
Brady Tatro <bradytatro@gmail.com>
2017-07-25 12:06:42 -0400

    fixed to wound logic for 6+, added in a break to reduce load times. Closed infitie loop for multiwound models. added logic for +1/-1 to hit for attacking models

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -34,6 +34,8 @@ class StaticPagesController < ApplicationController
       :wounds,
       :armour,
       :invulnerable,
+      :neg1_to_hit,
+      :plus1_to_hit,
       :times_run,
       :authenticity_token,
       :utf8,
@@ -44,11 +46,15 @@ class StaticPagesController < ApplicationController
   def battle(battle_data)
     @battle_data = battle_data
     @round = 0
-    while @battle_data["defend_models"].to_i > 0 do
+    @defending_models = @battle_data["defend_models"].to_i
+    while @defending_models > 0 do
       to_hit
       to_wound
       saving_throw
       @round += 1
+      if @battle_data["times_run"].to_i >= 10 && @round == 100
+        break
+      end
     end
     return @round
   end
@@ -56,7 +62,13 @@ class StaticPagesController < ApplicationController
   def to_hit
     @shots = @battle_data["attack_models"].to_i * @battle_data["shots"].to_i
     @rolled_shots = roll(@shots)
-    @rolled_shots.delete_if { |hit| hit < @battle_data["ballistic_skill"].to_i }
+    if @battle_data["plus1_to_hit"] == "1"
+      @rolled_shots.delete_if { |hit| hit + 1 < @battle_data["ballistic_skill"].to_i }
+    elsif @battle_data["neg1_to_hit"] == "1"
+      @rolled_shots.delete_if { |hit| hit - 1 < @battle_data["ballistic_skill"].to_i }
+    else
+      @rolled_shots.delete_if { |hit| hit < @battle_data["ballistic_skill"].to_i }
+    end
     @hits = @rolled_shots.length
   end
 
@@ -69,17 +81,18 @@ class StaticPagesController < ApplicationController
         @wounds.delete_if { |wound| wound < 3 }
       elsif @battle_data["weapon_strength"].to_i == @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 4 }
-      elsif @battle_data["weapon_strength"].to_i / 2 <= @battle_data["toughness"].to_i
+      elsif @battle_data["weapon_strength"].to_i <= @battle_data["toughness"].to_i / 2
         @wounds.delete_if { |wound| wound < 6 }
       elsif @battle_data["weapon_strength"].to_i < @battle_data["toughness"].to_i
         @wounds.delete_if { |wound| wound < 5 }
       end
     end
+    @rolled_wounds = @wounds.length
   end
 
   def saving_throw
-    if @wounds.length > 0
-      @saved = roll(@wounds.length)
+    if @rolled_wounds > 0
+      @saved = roll(@rolled_wounds)
       @original = @saved.length
       @saved.delete_if { |save| save < @battle_data["armour"].to_i && @battle_data["invulnerable"].to_i }
       if @battle_data["ap_value"].to_i > 0
@@ -94,17 +107,16 @@ class StaticPagesController < ApplicationController
       else
         @damaged = @original - @saved.length
       end
+      current_wounds = 0
       while @damaged > 0
-        current_wounds = 0
         current_wounds += @battle_data["damage"].to_i
           if current_wounds >= @battle_data["wounds"].to_i
-            @battle_data["defend_models"] = @battle_data["defend_models"].to_i - 1
+            @defending_models = @defending_models - 1
             current_wounds = 0
           end
-          @damaged -= 1
+        @damaged -= 1
       end
     end
-    return @battle_data["defend_models"].to_i
   end
 
   def roll(num)
