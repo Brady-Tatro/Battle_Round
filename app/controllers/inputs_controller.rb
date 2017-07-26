5afb57f3eb682a72237529f5b5baa2c3bfe85761
Brady Tatro <bradytatro@gmail.com>
2017-07-26 18:18:45 -0400

    broke into a module, cleaned up variable names

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,5 +1,7 @@
 class InputsController < ApplicationController
   require 'gchart'
+  require 'battleround.rb'
+  include BattleRound
 
   def index
     @battle = {}
@@ -42,92 +44,6 @@ class InputsController < ApplicationController
       :utf8,
       :commit
     )
-  end
-
-  def battle(battle_data)
-    @battle_data = battle_data
-    @round = 0
-    @defending_models = @battle_data["defend_models"].to_i
-    while @defending_models > 0 do
-      to_hit
-      to_wound
-      saving_throw
-      @round += 1
-      if @battle_data["times_run"].to_i >= 10 && @round == 100
-        break
-      end
-    end
-    return @round
-  end
-
-  def to_hit
-    @shots = @battle_data["attack_models"].to_i * @battle_data["shots"].to_i
-    @rolled_shots = roll(@shots)
-    if @battle_data["plus1_to_hit"] == "1"
-      @rolled_shots.delete_if { |hit| hit + 1 < @battle_data["ballistic_skill"].to_i }
-    elsif @battle_data["neg1_to_hit_attack"] || @battle_data["neg1_to_hit_defend"] == "1"
-      @rolled_shots.delete_if { |hit| hit - 1 < @battle_data["ballistic_skill"].to_i }
-    elsif @battle_data["neg1_to_hit_attack"] && @battle_data["neg1_to_hit_defend"] == "1"
-      @rolled_shots.delete_if { |hit| hit - 2 < @battle_data["ballistic_skill"].to_i }
-    else
-      @rolled_shots.delete_if { |hit| hit < @battle_data["ballistic_skill"].to_i }
-    end
-    @hits = @rolled_shots.length
-  end
-
-  def to_wound
-    if @hits > 0
-      @wounds = roll(@hits)
-      if @battle_data["weapon_strength"].to_i >= @battle_data["toughness"].to_i * 2
-        @wounds.delete_if { |wound| wound < 2 }
-      elsif @battle_data["weapon_strength"].to_i > @battle_data["toughness"].to_i
-        @wounds.delete_if { |wound| wound < 3 }
-      elsif @battle_data["weapon_strength"].to_i == @battle_data["toughness"].to_i
-        @wounds.delete_if { |wound| wound < 4 }
-      elsif @battle_data["weapon_strength"].to_i <= @battle_data["toughness"].to_i / 2
-        @wounds.delete_if { |wound| wound < 6 }
-      elsif @battle_data["weapon_strength"].to_i < @battle_data["toughness"].to_i
-        @wounds.delete_if { |wound| wound < 5 }
-      end
-    end
-    @rolled_wounds = @wounds.length
-  end
-
-  def saving_throw
-    if @rolled_wounds > 0
-      @saved = roll(@rolled_wounds)
-      @original = @saved.length
-      @saved.delete_if { |save| save < @battle_data["armour"].to_i && @battle_data["invulnerable"].to_i }
-      if @battle_data["ap_value"].to_i > 0
-        if @battle_data["invulnerable"].to_i > @battle_data["armour"].to_i - @battle_data["ap_value"].to_i
-          @saved.delete_if { |save| save < @battle_data["invulnerable"].to_i  }
-        else
-          @saved.delete_if { |save| save - @battle_data["ap_value"].to_i < @battle_data["armour"].to_i  }
-        end
-      end
-      if @saved.nil?
-        @damaged = @original
-      else
-        @damaged = @original - @saved.length
-      end
-      current_wounds = 0
-      while @damaged > 0
-        current_wounds += @battle_data["damage"].to_i
-          if current_wounds >= @battle_data["wounds"].to_i
-            @defending_models = @defending_models - 1
-            current_wounds = 0
-          end
-        @damaged -= 1
-      end
-    end
-  end
-
-  def roll(num)
-    result = []
-    num.times do
-      result << rand(6) + 1
-    end
-    return result
   end
 
   def aggregation(array)
