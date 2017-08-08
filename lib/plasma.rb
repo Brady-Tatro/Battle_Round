0d771d8344f513441b5e07c47d7aff0e4ea13fa4
Brady Tatro <bradytatro@gmail.com>
2017-08-08 12:52:28 -0400

    working on plasma hit

--- lib/plasma.rb
+++ lib/plasma.rb
@@ -1,8 +1,13 @@
 module Plasma
 
   def plasma_to_hit
-    
+    models_to_shoot = @attacking_models
+    while models_to_shoot > 0
+      total_hits = 0
+      rolled_shots << roll(@shots)
+      
 
+    end
   end
 
 end
