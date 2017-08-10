3620509117dfc759a8c5a1f70a1b4ec73fa04007
Brady Tatro <bradytatro@gmail.com>
2017-08-10 12:26:18 -0400

    rerolling slowly adding

--- lib/plasma.rb
+++ lib/plasma.rb
@@ -2,12 +2,36 @@ module Plasma
 
   def plasma_to_hit
     models_to_shoot = @attacking_models
+    total_hits = 0
     while models_to_shoot > 0
-      total_hits = 0
+      ones_rolled = 0
+      rerolled_shots = 0
       rolled_shots << roll(@shots)
-      
-
+      rolled_shots.each do |check|
+        if check == 1
+          ones_rolled += 1
+        end
+      end
+      if @battle_data["reroll1_hits"] == 1
+        rolled_shots.each do |ones|
+          if ones == 1
+            rerolled_shots += 1
+            ones_rolled += 1
+          end
+        end
+      elsif @battle_data["reroll_hits"] == 1
+        rolled_shots.each do |misses|
+          if misses < @ballistic_skill
+            if misses == 1
+              rerolled_shots += 1
+              ones_rolled += 1
+            else
+              rerolled_shots += 1
+            end
+          end
+        end
+      end
     end
   end
-
+  neg_one
 end
