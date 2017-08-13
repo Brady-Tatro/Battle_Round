53cce8d9c703fa3599a15539c7343f03a9c131d3
Brady Tatro <bradytatro@gmail.com>
2017-08-13 18:23:37 -0400

    while loop should be working with rerolling

--- lib/plasma.rb
+++ lib/plasma.rb
@@ -1,37 +1,68 @@
 module Plasma
 
   def plasma_to_hit
-    models_to_shoot = @attacking_models
-    total_hits = 0
-    while models_to_shoot > 0
-      ones_rolled = 0
-      rerolled_shots = 0
-      rolled_shots << roll(@shots)
-      rolled_shots.each do |check|
-        if check == 1
-          ones_rolled += 1
+    @models_to_shoot = @attacking_models
+    @total_hits = 0
+
+    while @models_to_shoot > 0
+      @ones_rolled = false
+      @rerolled_shots = [0]
+      @rolled_shots = roll(@shots)
+      rerolling
+      one_checking_no_reroll
+      accumilation
+    end
+
+  end
+
+  def one_checking_no_reroll
+    @rolled_shots.each do |check|
+      if check == 1
+        @ones_rolled = true
+        break
+      end
+    end
+    if @ones_rolled == true
+      @models_to_shoot -= 1
+      @ones_rolled = false
+      @rolled_shots = [0]
+    end
+  end
+
+  def one_checking_reroll
+    @rerolled.each do |check|
+      if check == 1
+        @ones_rolled = true
+        break
+      end
+    end
+    if @ones_rolled == true
+      @models_to_shoot -= 1
+      @ones_rolled = false
+      @rolled_shots = [0]
+      @rerolled_shots = [0]
+    end
+  end
+
+  def rerolling
+    if @battle_data["reroll1_hits"] == 1
+      @rolled_shots.each do |ones|
+        if ones == 1
+          @rerolled_shots += 1
         end
       end
-      if @battle_data["reroll1_hits"] == 1
-        rolled_shots.each do |ones|
-          if ones == 1
-            rerolled_shots += 1
-            ones_rolled += 1
-          end
-        end
-      elsif @battle_data["reroll_hits"] == 1
-        rolled_shots.each do |misses|
-          if misses < @ballistic_skill
-            if misses == 1
-              rerolled_shots += 1
-              ones_rolled += 1
-            else
-              rerolled_shots += 1
-            end
-          end
+    elsif @battle_data["reroll_hits"] == 1
+      @rolled_shots.each do |misses|
+        if misses < @ballistic_skill
+          @rerolled_shots += 1
         end
       end
     end
+    @rerolled = roll(@rerolled_shots)
+    one_checking_reroll
   end
-  neg_one
+
+  def accumilation
+    @total_shots = (@rolled_shots << @rerolled_shots).flatten!
+  end
 end
