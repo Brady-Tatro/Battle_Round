f5facddc6c0b6da1a8f648dbdd74d1993d16d3ba
Brady Tatro <bradytatro@gmail.com>
2017-12-30 18:03:40 -0500

    wound function takes into account mortal wounds, created variable to allow for smite in the future

--- lib/savingthrow.rb
+++ lib/savingthrow.rb
@@ -13,27 +13,32 @@ module SavingThrow
         end
       end
       if @saved.nil?
-        @unsaved_wounds = @original
+        unsaved_wounds = @original
       else
-        @unsaved_wounds = @original - @saved.length
+        unsaved_wounds = @original - @saved.length
+        resolve_damage(@combat_mortal, true)
+        resolve_damage(unsaved_wounds, false)
       end
-      current_wounds = 0
-      @died_this_round = 0
-      while @unsaved_wounds > 0
-        if @battle_data["d3_damage"] == "1"
-          current_wounds += d3
-        elsif @battle_data["d6_damage"] = "1"
-          current_wounds += d6
-        else
+    end
+  end
+
+  def resolve_damage(total_wounds, mortal)
+    current_wounds = 0
+    @died_this_round = 0
+    while total_wounds > 0
+      if @battle_data["d3_damage"] == "1" && mortal == false
+        current_wounds += d3
+      elsif @battle_data["d6_damage"] = "1" && mortal == false
+        current_wounds += d6
+      else
         current_wounds += @damage
-        end
-          if current_wounds >= @hp
-            @defending_models -= 1
-            @died_this_round += 1
-            current_wounds = 0
-          end
-        @unsaved_wounds -= 1
       end
+      if current_wounds >= @hp
+        @defending_models -= 1
+        @died_this_round += 1
+        current_wounds = 0
+      end
+      total_wounds -= 1
     end
   end
 
