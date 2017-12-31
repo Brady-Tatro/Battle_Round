291b682be8b6cac2b99503bf3694b86cf092f612
Brady Tatro <bradytatro@gmail.com>
2017-12-30 19:18:18 -0500

    basic scheme for form set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171230235051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sumbissions", force: :cascade do |t|
+    t.integer "attack_models",      null: false
+    t.integer "shots",              null: false
+    t.integer "ballistic_skill",    null: false
+    t.integer "weapon_strength",    null: false
+    t.integer "damage",             null: false
+    t.integer "ap_value",           null: false
+    t.integer "defend_models",      null: false
+    t.integer "toughness",          null: false
+    t.integer "armor",              null: false
+    t.integer "invulnerable",       null: false
+    t.integer "leadership",         null: false
+    t.integer "times_run",          null: false
+    t.string  "d3"
+    t.string  "d6"
+    t.string  "plasma"
+    t.string  "always_hit"
+    t.string  "sniper"
+    t.string  "neg1_to_hit_attack"
+    t.string  "neg1_to_hit_defend"
+    t.string  "plus1_to_hit"
+    t.string  "reroll_hits"
+    t.string  "reroll1_hits"
+    t.string  "reroll_wounds"
+    t.string  "reroll1_wounds"
+  end
 
 end
